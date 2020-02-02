022c89e813fa8e4edd0bf7a45557c9c40a54c725
KosukeShimizu <thimi0412@icloud.com>
2020-02-02 23:18:36 +0900

    frst commit

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "google" {
+  credentials = "${file("credentials.json")}"
+  project     = "shimizu-test-20200202"
+  region      = "asia-northeast1"
+  zone        = "asia-northeast1-a"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+
+    }
+  }
+}
