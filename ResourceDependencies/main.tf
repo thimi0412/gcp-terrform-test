2a84a59a0da0bf02e54abe6cfea22d6d8d0773d8
KosukeShimizu <thimi0412@icloud.com>
2020-02-09 00:08:08 +0900

    add provisioner

--- ResourceDependencies/main.tf
+++ ResourceDependencies/main.tf
@@ -18,6 +18,10 @@ resource "google_compute_instance" "vm_instance" {
   machine_type = "f1-micro"
   tags         = ["web", "dev"]
 
+  provisioner "local-exec" {
+    command = "echo ${google_compute_instance.vm_instance.name}:  ${google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip} >> ip_address.txt"
+  }
+
   boot_disk {
     initialize_params {
       image = "cos-cloud/cos-stable"
@@ -28,35 +32,6 @@ resource "google_compute_instance" "vm_instance" {
     network = google_compute_network.vpc_network.name
     access_config {
       nat_ip = google_compute_address.vm_tatic_ip.address
-    }
-  }
-}
-
-resource "google_storage_bucket" "example_bucket" {
-  name     = "terraform-example-bucket-shimizh-20200202"
-  location = "ASIA"
-
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
-}
-
-resource "google_compute_instance" "another_instance" {
-  depends_on = [google_storage_bucket.example_bucket]
-
-  name         = "terraform-instance-2"
-  machine_type = "f1-micro"
-
-  boot_disk {
-    initialize_params {
-      image = "cos-cloud/cos-stable"
-    }
-  }
-
-  network_interface {
-    network = google_compute_network.vpc_network.self_link
-    access_config {
     }
   }
 }
